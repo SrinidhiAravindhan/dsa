98833810856df9f2a6e4f2ad236f946f950700ad
SrinidhiAravindhan <srinidhiaravin@gmail.com>
2024-12-24 10:18:22 +0530

    Create binarysearch tree.java

---
+++ binarysearch tree.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numTrees(int n) {
+        
+        int[] dp = new int[n + 1];
+
+        
+        dp[0] = 1;
+        dp[1] = 1;
+
+        // Fill dp array for 2 to n nodes
+        for (int nodes = 2; nodes <= n; nodes++) {
+            for (int root = 1; root <= nodes; root++) {
+                
+                int leftSubtrees = dp[root - 1];
+                int rightSubtrees = dp[nodes - root];
+                dp[nodes] += leftSubtrees * rightSubtrees;
+            }
+        }
+
+        return dp[n];
+    }
+}
